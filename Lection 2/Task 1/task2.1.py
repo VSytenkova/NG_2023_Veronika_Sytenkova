877d1343588766cf8cc64226ea2b64a665a2d886
unknown <kin77444@gmail.com>
2023-11-24 15:20:02 +0200

    Changed first task of the second lection

--- Lection 2/Task 1/task2.1.py
+++ Lection 2/Task 1/task2.1.py
@@ -1,9 +1,8 @@
-user_list = [];
-quanity = int(input("How many elements do you wanna add?: "));
-while quanity > 0:
-    element = input("Your element: ")
+user_list = []
+element = str
+while element != 'stop':
+    element = input("Enter your element: ")
     user_list.append(element)
-    quanity -= 1;
-list_set = set(user_list);
+list_set = set(user_list)
 unique_list = list(list_set)
 print(unique_list)
