9bca1b7baed829bd7d6d98baa3231c996d0b8e30
unknown <kin77444@gmail.com>
2023-11-24 19:29:40 +0200

    Fixed second and third tasks of second lection.

--- Lection 2/Task 1/task2.1.py
+++ Lection 2/Task 1/task2.1.py
@@ -1,7 +1,7 @@
 user_list = []
 element = str
 while element != 'stop':
-    element = input("Enter your element: ")
+    element = input("Enter your element, but if u wanna stop enter stop: ")
     user_list.append(element)
 list_set = set(user_list)
 unique_list = list(list_set)
--- Lection 2/Task 3/task2.3.py
+++ Lection 2/Task 3/task2.3.py
@@ -1,15 +1,15 @@
 max_number = int(input("Enter a max number: "))
 divider_nmbrs = []
 prime_nmbrs = []
-qurent_number = 0
-while qurent_number < max_number: 
-    qurent_number += 1
+current_number = 0
+while current_number < max_number: 
+    current_number += 1
     divider = 1   
-    while divider <= qurent_number:
-        if (qurent_number % divider) == 0:
+    while divider <= current_number:
+        if (current_number % divider) == 0:
             divider_nmbrs.append(divider)
         divider += 1
-    string = "{qurent}\t | {dividers}\n".format(qurent = qurent_number, dividers = divider_nmbrs)
+    string = "{current}\t | {dividers}\n".format(current = current_number, dividers = divider_nmbrs)
     print(string)
     if len(divider_nmbrs) == 2:
         prime_nmbrs.append(divider_nmbrs[-1])
